09a05e7532d3a436b306b132c5383b4801cc921b
Sebastien Kister <sebastien@arianesline.com>
2025-03-04 15:25:29 -0600

    Added transitive require

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.arianesline.cavelib.api {
     exports com.arianesline.cavelib.api;
 
     requires javafx.base;
-    requires javafx.graphics;
+    requires transitive javafx.graphics;
     requires javafx.controls;
+
 }
